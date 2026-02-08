2143ed6edea9061314d527440a38ba5aebecbc6f
Simon Marquis <contact@simon-marquis.fr>
2026-02-08 13:03:39 +0100

    🧹 Cleanup project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,9 +11,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 plugins {
+    id("com.gradle.develocity") version "4.3.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -22,5 +26,23 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Lint Playground"
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        val isCi = providers.environmentVariable("CI").map(String::toBoolean)
+        publishing.onlyIf { isCi.getOrElse(false) }
+        uploadInBackground = isCi
+        obfuscation {
+            ipAddresses { it.map { "0.0.0.0" } }
+        }
+    }
+}
+
+// https://docs.gradle.org/current/userguide/configuration_cache.html#config_cache:stable
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "Lint-Playground"
+
 include(":app")
